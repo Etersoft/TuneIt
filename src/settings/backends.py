a307d82703024ceeecf9ae37bd71b227276548cb
Roman Alifanov <ximper@etersoft.ru>
2025-01-26 01:23:29 +0300

    init BinaryBackend

--- src/settings/backends.py
+++ src/settings/backends.py
@@ -1,6 +1,6 @@
 from gi.repository import Gio, GLib
 import os, re
-
+import ast, subprocess
 
 class Backend:
     def __init__(self, params=None):
@@ -184,11 +184,77 @@ class FileBackend(Backend):
 
         self._save_file()
 
+class BinaryBackend(Backend):
+    def __init__(self, params=None):
+        super().__init__(params)
+        self.binary_path = os.path.join(
+            self.params.get('module_path'),
+            self.params.get('binary_path')
+        )
+
+        self.binary_name = self.params.get('binary_name')
+
+    def _run_binary(self, command, *args):
+        try:
+            full_command = (
+                [self.binary_path + self.binary_name, command]
+                + [x for x in args if x is not None]
+            )
+
+            result = subprocess.run(full_command, capture_output=True, text=True, check=True)
+
+            return result.stdout.strip()
+        except subprocess.CalledProcessError as e:
+            print(f"[ERROR] Ошибка при выполнении команды {command}: {e}")
+            return None
+
+    def get_value(self, key, gtype):
+        print(f"[DEBUG] Получение значения: key={key}, gtype={gtype}")
+
+        result = self._run_binary('get_value', key)
+
+        if result:
+            try:
+                return ast.literal_eval(result)
+            except (ValueError, SyntaxError) as e:
+                print(f"[ERROR] Ошибка при преобразовании результата {result}: {e}")
+                return result
+        return None
+
+    def get_range(self, key, gtype):
+        print(f"[DEBUG] Получение диапазона: key={key}, gtype={gtype}")
+
+        result = self._run_binary('get_range', key)
+
+        if not result:
+            print(f"[ERROR] Пустой результат или ошибка при выполнении команды get_range для ключа {key}")
+            return None
+
+        try:
+            parsed_result = ast.literal_eval(result)
+            return parsed_result
+        except (ValueError, SyntaxError) as e:
+            print(f"[ERROR] Ошибка при преобразовании результата {result} для ключа {key}: {e}")
+            return None
+
+    def set_value(self, key, value, gtype):
+        print(f"[DEBUG] Установка значения: key={key}, value={value}, gtype={gtype}")
+
+        result = self._run_binary('set_value', key, str(value))
+
+        if result:
+            try:
+                return ast.literal_eval(result)
+            except (ValueError, SyntaxError) as e:
+                print(f"[ERROR] Ошибка при преобразовании результата {result}: {e}")
+        return None
+
 class BackendFactory:
     def __init__(self):
         self.backends = {
             'gsettings': GSettingsBackend,
             'file': FileBackend,
+            'binary': BinaryBackend,
         }
 
     def get_backend(self, backend_name, params=None):
@@ -205,6 +271,8 @@ class RootBackendFactory:
     def __init__(self):
         self.backends = {
             'file': FileBackend,
+            'binary': BinaryBackend,
+
         }
 
     def get_backend(self, backend_name, params=None):
